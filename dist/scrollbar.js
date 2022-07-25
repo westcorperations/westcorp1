bdff4da4386dfbfed06e36a5eca79136c317812f
westlove <ignaciowestlove@gmail.com>
2022-07-25 01:16:44 +0100

    skill2

--- dist/scrollbar.js
+++ dist/scrollbar.js
@@ -24,3 +24,59 @@
  
     setTimeout( progress, 2000 );
   } );
+
+
+ $( function() {
+    var progressbar = $( "#progressbar3" ),
+      progressLabel = $( ".progress-label3" );
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "60%" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + 2 );
+ 
+      if ( val < 65 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+ 
+    setTimeout( progress, 2000 );
+  } );
+
+
+ $( function() {
+    var progressbar = $( "#progressbar2" ),
+      progressLabel = $( ".progress-label2" );
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "78%" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + 2 );
+ 
+      if ( val < 80 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+ 
+    setTimeout( progress, 2000 );
+  } );
